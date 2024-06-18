e222c080660bb3487d7c33159d5c2af147fe7cf9
luizgdpo <121655908+luizgdpo@users.noreply.github.com>
2024-06-18 20:09:53 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+//Not a WIP anymore!
+    document.getElementById('go').onclick = function(){
+    let searchQuery = document.getElementById('q').value;
+    window.location.href = 'https://google.com/search?q='+searchQuery;
+
+    }
